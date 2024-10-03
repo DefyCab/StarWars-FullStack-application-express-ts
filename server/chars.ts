d7d4aaa3e5d134ef78396611b925899c953346b6
defycab <love.bellinder@gmail.com>
2024-10-03 21:57:04 +0200

    add fetch to get characters

--- server/chars.ts
+++ server/chars.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Character List');
+router.get('/', async (req, res) => {
+  try {
+    const response = await fetch(`https://swapi.dev/api/people/`);
+    const data = (await response.json()) as { results: Chars };
+  } catch (error) {
+    res.status(500).json({ error: 'Server failure' });
+  }
 });
 
 module.exports = router;
