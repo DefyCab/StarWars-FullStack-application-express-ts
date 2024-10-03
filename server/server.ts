b3d3e208e4c04457511c2963c8021c0cb26f7171
defycab <love.bellinder@gmail.com>
2024-10-03 21:38:26 +0200

    set up character router

--- server/server.ts
+++ server/server.ts
@@ -6,9 +6,13 @@ const port = 3000;
 
 app.use(cors());
 
-app.get('/chars', (req, res) => {
-  res.send('Welcome to my server!');
+app.get('/', (req, res) => {
+  res.send('Welcome to the Star Wars server!');
 });
+
+const charRouter = require('./chars');
+
+app.use('/chars', charRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
